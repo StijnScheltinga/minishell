b4ec66b1fee933e96c0d4b7243d88fe74bcf3980
AlexOldeMonnikhof <a.oldemonnikhof@gmail.com>
2023-07-10 19:36:26 +0200

    new exit

--- src/builtins/ms_exit.c
+++ src/builtins/ms_exit.c
@@ -6,13 +6,33 @@
 /*   By: aolde-mo <aolde-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 18:11:48 by aolde-mo          #+#    #+#             */
-/*   Updated: 2023/06/27 18:14:04 by aolde-mo         ###   ########.fr       */
+/*   Updated: 2023/07/10 18:08:24 by aolde-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/builtin.h"
 
-void	ms_exit(int error)
+static int	is_int(char *s)
 {
-	exit(error);
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (!ft_isdigit(s[i++]))
+			return (0);
+	}
+	return (1);
+}
+
+void	ms_exit(char **arg)
+{
+	if (!arg[1])
+		exit(0);
+	if (!is_int(arg[1]))
+	{
+		dprintf(2, "moet nummer zijn\n");
+		exit(255);
+	}
+	exit(ft_atoi(arg[1]));
 }
