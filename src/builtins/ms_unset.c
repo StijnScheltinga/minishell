421effbe35e3544342bf73e0f269cd62cdcce30d
AlexOldeMonnikhof <a.oldemonnikhof@gmail.com>
2023-07-13 18:25:48 +0200

    beginning of unset

--- src/builtins/ms_unset.c
+++ src/builtins/ms_unset.c
@@ -3,23 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ms_unset.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sschelti <sschelti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aolde-mo <aolde-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 18:06:53 by aolde-mo          #+#    #+#             */
-/*   Updated: 2023/07/07 13:25:43 by sschelti         ###   ########.fr       */
+/*   Updated: 2023/07/13 17:13:26 by aolde-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/builtin.h"
+#include "../../inc/env.h"
 
+static void	unset_cmd(t_env *env, char *arg)
+{
+	
+}
 
-//uses envp from seperate int main -> no other envp yet
+void	ms_unset(t_cmd_table *cmd_table, char **cmd)
+{
+	int	i;
 
-//does not return error code but prints the error
-
-// void	ms_unset(envp);
-
-// int main(int argc, char **argv, char **envp)
-// {
-
-// }
+	i = 1;
+	if (!cmd[i])
+		return ;
+	while (cmd[i])
+		unset_cmd(cmd_table->env, cmd[i++]);
+}
