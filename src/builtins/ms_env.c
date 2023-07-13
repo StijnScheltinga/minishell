403e6d37254b264404881e86c20d29ef4b814e5a
AlexOldeMonnikhof <a.oldemonnikhof@gmail.com>
2023-07-13 18:25:27 +0200

    new env linked list

--- src/builtins/ms_env.c
+++ src/builtins/ms_env.c
@@ -6,19 +6,19 @@
 /*   By: aolde-mo <aolde-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 18:03:09 by aolde-mo          #+#    #+#             */
-/*   Updated: 2023/06/27 18:06:43 by aolde-mo         ###   ########.fr       */
+/*   Updated: 2023/07/13 16:13:07 by aolde-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/builtin.h"
 
-void	ms_env(char **envp)
+void	ms_env(t_env *head)
 {
-	unsigned int	i;
-
-	i = 0;
-	while (envp[i])
-		printf("%s\n", envp[i++]);
+	while (head)
+	{
+		printf("%s%s\n", head->variable, head->value);
+		head = head->next;
+	}
 }
 
 // int	main(int argc, char **argv, char **envp)
