aa0dbb9e450ae4d0e229ae7b3e8be0d0fa594e8a
alex <a.oldemonnikhof@gmail.com>
2023-09-12 15:49:44 +0100

    cd errors

--- inc/builtin.h
+++ inc/builtin.h
@@ -6,7 +6,7 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 15:14:55 by aolde-mo          #+#    #+#             */
-/*   Updated: 2023/09/11 20:28:34 by alex             ###   ########.fr       */
+/*   Updated: 2023/09/12 14:09:33 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ bool	is_builtin(char *arg);
 void	execute_builtin(t_cmd_table *cmd_table, int cmd_index);
 
 void	echo(t_cmd_table *cmd_table, char **arg);
-void	cd(char *arg, t_env **env_head, t_cmd_table *cmd_table);
+void	cd(char **arg, t_env **env_head, t_cmd_table *cmd_table);
 void	env(t_cmd_table *cmd_table, t_env **head);
 void	ms_exit(char **arg, t_cmd_table *cmd_table);
 void	export(t_cmd_table *cmd_table, char **cmd);
--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 15:48:24 by aolde-mo          #+#    #+#             */
-/*   Updated: 2023/09/11 20:14:17 by alex             ###   ########.fr       */
+/*   Updated: 2023/09/12 15:41:27 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,15 @@
 #include <string.h>
 
 //not implementing error code
+
+static void	error_msg(t_cmd_table* cmd_table, char *arg, char *error_msg)
+{
+	ft_putstr_fd(arg, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(error_msg, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	cmd_table->latest_exit_code = 1;
+}
 
 char	*get_home(t_env **env_head, t_cmd_table *cmd_table)
 {
@@ -46,42 +55,39 @@ char	*get_home(t_env **env_head, t_cmd_table *cmd_table)
 
 //uses envp from seperate int main -> no other envp yet
 
-static char	*find_right_path(char *arg, t_env **env_head, t_cmd_table *cmd_tab)
+static char	*find_right_path(char **arg, t_env **env_head, t_cmd_table *cmd_tab)
 {
 	char	*path;
 
 	path = NULL;
-	if (arg == NULL)
+	if (arg[1] == NULL)
 		path = get_home(env_head, cmd_tab);
-	else if (ft_strncmp(arg, "~", sizeof(arg)) == 0)
+	else if (ft_strncmp(arg[1], "~", sizeof(arg[1])) == 0)
 		path = cmd_tab->home;
 	else
-		path = arg;
+		path = arg[1];
 	return (path);
 }
 
 //does not return error code but prints the error
 
-void	cd(char *arg, t_env **env_head, t_cmd_table *cmd_table)
+void	cd(char **arg, t_env **env_head, t_cmd_table *cmd_table)
 {
 	char	*path;
 
+	if (arg[1])
+	{
+		if (arg[2])
+		{
+			error_msg(cmd_table, arg[0], "too many arguments");
+			return ;
+		}
+	}
 	path = find_right_path(arg, env_head, cmd_table);
 	if (!path)
 		return ;
 	if (chdir(path))
-	{
-		ft_putstr_fd(arg, STDERR_FILENO);
-		ft_putstr_fd(": ", STDERR_FILENO);
-		ft_putstr_fd(strerror(errno), STDERR_FILENO);
-		ft_putstr_fd("\n", STDERR_FILENO);
-		cmd_table->latest_exit_code = 1;
-	}
+		error_msg(cmd_table, arg[1], strerror(errno));
 	else
 		cmd_table->latest_exit_code = 0;
 }
-
-// int main(int argc, char **argv, char **envp)
-// {
-// 	cd(NULL, envp);
-// }
